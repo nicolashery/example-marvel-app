8299b44e1853b82894073b766760bcda58f8aa83
Nicolas Hery <hery.nicolas@gmail.com>
2016-03-30 19:50:27 -0400

    Add browser-refresh

--- app/start.js
+++ app/start.js
@@ -1,7 +1,14 @@
+require('marko/browser-refresh').enable();
+
 var app = require('./app');
 
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log('App listening on port ' + PORT);
+
+  // Tell browser-refresh that server is ready to receive requests
+  if (process.send) {
+    process.send('online');
+  }
 });

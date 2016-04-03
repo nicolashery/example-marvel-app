f7e43ac8d353dc3fddbc0e30495a63fb0ff61954
Nicolas Hery <hery.nicolas@gmail.com>
2016-04-03 16:11:06 -0400

    Add NProgress bar

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,3 +1,28 @@
-/* global spf */
+/* global spf, NProgress */
 
-spf.init();
+var app = app || {};
+
+app.init = function() {
+  if (window.addEventListener) {
+    window.addEventListener('spfrequest', app.onSpfRequest);
+    window.addEventListener('spfdone', app.onSpfDone);
+  }
+  app.enabled = spf.init();
+};
+
+app.dispose = function() {
+  if (window.removeEventListener) {
+    window.removeEventListener('spfrequest', app.onSpfRequest);
+    window.removeEventListener('spfdone', app.onSpfDone);
+  }
+};
+
+app.onSpfRequest = function() {
+  NProgress.start();
+};
+
+app.onSpfDone = function() {
+  NProgress.done();
+};
+
+app.init();
